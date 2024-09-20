8916a48cf067496e6f5e4c945dffcee195418f88
thunyP <tpt8669@gmail.com>
2024-09-19 21:49:26 -0500

    displayed image

--- base/src/main.jsx
+++ base/src/main.jsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client'
-// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,7 +10,6 @@ import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import Portfolio from './pages/Portfolio';
 
-// Define the accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
     path: '/',
